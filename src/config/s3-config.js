0790efb52d2bc8c53b380055ad9892a7519e8013
rahullkr <rk7318323176@gmail.com>
2023-11-14 21:21:20 +0530

    added image upload in tweet

---
+++ src/config/s3-config.js
@@ -0,0 +1,28 @@
+import multer from "multer";
+import multers3 from "multer-s3";
+import aws from "aws-sdk";
+import dotenv from "dotenv";
+dotenv.config();
+aws.config.update({
+  region: process.env.AWS_REGION,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+});
+
+const s3 = new aws.S3();
+
+const upload = multer({
+  storage: multers3({
+    s3: s3,  
+    bucket: process.env.BUCKET_NAME,
+    // acl: "public-read",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
+
+export default upload; 
--- src/controllers/tweet-controller.js
+++ src/controllers/tweet-controller.js
@@ -1,9 +1,19 @@
 import TweetService from "../services/tweet-service.js";
 
+import upload from '../config/s3-config.js'
+const singleUploader = upload.single('image')
 const tweetService = new TweetService();  
 export const createTweet = async(req,res) => {
     try {
-        const response = await tweetService.create(req.body); 
+        singleUploader(req,res, async function (err, data){
+            if(err){
+                return res.status(500).json({
+                    error: err
+                })
+            }
+         const payload = req.body; 
+         payload.image = req.file.location ; 
+        const response = await tweetService.create(payload); 
         res.status(201).json({
             success: true, 
             message: 'successfully cretead a new tweet',
@@ -11,13 +21,22 @@ export const createTweet = async(req,res) => {
             err: {},
 
         })
+        })
+
+        // const response = await tweetService.create(req.body); 
+        // res.status(201).json({
+        //     success: true, 
+        //     message: 'successfully cretead a new tweet',
+        //     data: response,
+        //     err: {},
+
+        // })
     } catch (error) {
           res.status(500).json({
             success: false, 
             message: 'something went wrong',
             data: {},
-            err: error,
-            
+            err: error,  
         })
     }
 }
